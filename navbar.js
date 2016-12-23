e348eed7e1e993f4476ef3aa6766c6592851774c
Sheila Lo <lo.sheila@gmail.com>
2016-12-23 14:05:55 -0800

    Adding google analytics

--- navbar.js
+++ navbar.js
@@ -7,6 +7,16 @@ if ($(window).width() < 768) {
   var caret = '<span class="caret"></span>';
 }
 
+// Google Analytics script
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+ga('create', 'UA-89427036-1', 'auto');
+ga('send', 'pageview');
+
+
 document.getElementById("navMenu").innerHTML =
   '<nav class="navbar navbar-default navbar-fixed-top">' +
     '<div class="container">' +
