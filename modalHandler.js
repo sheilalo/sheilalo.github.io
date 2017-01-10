392f9fc9119194337edc9506f4492e6c1960f151
Sheila Lo <lo.sheila@gmail.com>
2017-01-10 00:46:46 -0800

    Redone about me. Modal images now use Ekko lightbox

--- modalHandler.js
+++ modalHandler.js
@@ -1,36 +1,18 @@
-// Start looking for clicks once the document loads
+$(document).delegate('*[data-toggle="lightbox"]', 'click', function(event) {
+    event.preventDefault();
+    $(this).ekkoLightbox({always_show_close: false});
+});
+
 $(document).ready(function() {
-    // If an image with class "myImg" is clicked
-    $('img.contentImg').click(function() {
-        // Get some references to things
-        var modal = document.getElementById("imgModal");
-        var modalImg = document.getElementById("image");
-        var captionText = document.getElementById("modal-caption");
 
-        // Show the modal dialog
-        modal.style.display = "block";
-        // Set the image in the dialog to the image that was clicked on
-        modalImg.src = this.src;
-        // Set the caption to the Alt-text for the image that was clicked on
-        captionText.innerHTML = this.alt;
-
-        // Setup refernce to close button
-        var span = document.getElementsByClassName("close")[0];
-
-        // When the user clicks on <span> (x), close the modal
-        span.onclick = function() { 
-            modal.style.display = "none";
-        }
-    });
-
-    // If the modal thing is clicked
-    $('div.modal').click(function() {
-        // Hide the modal thing
-        this.style.display = "none";
-    }).children().click(function(e) {
-        // If the thing that was clicked is a child of the modal dialog, do nothing
-        // (like if you click the picture or the caption)
-        return false;
-    });
-
+$('.contentImg').each(function () {
+    $(this).parent().css({
+        'height': $(this).height(),
+        'width': $(this).width(),
+        'display': 'inline-block'
+    })
+}).css({
+    'max-height': '100%',
+    'max-width': '100%'
+});
 });
