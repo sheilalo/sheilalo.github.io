e8bdb1d3da56e19a41b78fef7e5adad631a12885
Sheila Lo <lo.sheila@gmail.com>
2017-07-31 22:38:23 -0500

    Revert "Updated homepage to center vertically"
    This reverts commit 5e7f59e32f4ac560b128088cf4f06f7b2793778a.

--- home.js
+++
@@ -1,5 +0,0 @@
-/* Resize homecontainer div to fill width of screen */
-$(window).on('resize', function(){
-    var width = $(".container").width();
-    $("#homecontainer").width(width);
-}).resize();
